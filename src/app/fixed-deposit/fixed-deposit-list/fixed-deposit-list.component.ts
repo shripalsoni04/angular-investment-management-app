dc5d3fce4dbc09207c284770e5d8b4b6f698eca6
Shripal Soni <shripalsoni04@gmail.com>
2017-07-26 02:42:43 +0530

    feat(FD-list): Integrate edit/delete functionality and show busy-loader

--- src/app/fixed-deposit/fixed-deposit-list/fixed-deposit-list.component.ts
+++ src/app/fixed-deposit/fixed-deposit-list/fixed-deposit-list.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DataSource } from '@angular/cdk';
+import { MdDialog } from '@angular/material';
 
-import { AppTitleService, FixedDepositService } from '../../core/services';
+import { AppTitleService, FixedDepositService, DialogService } from '../../core/services';
 import { FixedDeposit } from '../../shared/models';
+import { CreateFixedDepositModalComponent } from '../create-fixed-deposit-modal/create-fixed-deposit-modal.component';
 
 @Component({
   selector: 'app-fixed-deposit-list',
@@ -16,38 +18,61 @@ export class FixedDepositListComponent implements OnInit {
   displayedColumns = ['fdNumber', 'bankName', 'accountNo', 'amount', 'startDate', 'endDate', 'isReinvest', 'actions'];
 
   columnConfig = [
-    { id: 'fdNumber', name: 'FD Number'},
-    { id: 'bankName', name: 'Bank Name'},
-    { id: 'accountNo', name: 'A/C No.'},
-    { id: 'amount', name: 'Amount', align: 'right', width: 'auto'},
-    { id: 'startDate', name: 'Start Date', align: 'center', width: '100px'},
-    { id: 'endDate', name: 'End Date', align: 'center', width: '100px'},
-    { id: 'isReinvest', name: 'Re-invest', align: 'center', width: '60px'},
-    { id: 'actions', name: 'Actions', align: 'center', width: '90px'}
+    { id: 'fdNumber', name: 'FD Number' },
+    { id: 'bankName', name: 'Bank Name' },
+    { id: 'accountNo', name: 'A/C No.' },
+    { id: 'amount', name: 'Amount', align: 'right', width: 'auto' },
+    { id: 'startDate', name: 'Start Date', align: 'center', width: '100px' },
+    { id: 'endDate', name: 'End Date', align: 'center', width: '100px' },
+    { id: 'isReinvest', name: 'Re-invest', align: 'center', width: '60px' },
+    { id: 'actions', name: 'Actions', align: 'center', width: '90px' }
   ];
+
+  isBusy = false;
 
   constructor(
     private appTitleService: AppTitleService,
-    private fdService: FixedDepositService
+    private mdDialog: MdDialog,
+    private dialogService: DialogService,
+    public fdService: FixedDepositService
   ) {
     this.appTitleService.setTitle('Fixed Deposits');
   }
 
   ngOnInit() {
     this.dataSource = new FDDataSource(this.fdService);
-    this.fdService.getFixedDeposits();
+    this.isBusy = true;
+    this.fdService.getFixedDeposits().subscribe(() => {
+      this.isBusy = false;
+    }, () => {
+      this.isBusy = false;
+    });
   }
 
   addFD() {
-
+    this.showFDFormModal();
   }
 
-  editFD() {
-
+  editFD(fdModel: FixedDeposit) {
+    this.showFDFormModal(fdModel.id);
   }
 
-  deleteFD() {
+  deleteFD(fdModel: FixedDeposit) {
+    this.dialogService.confirmDelete().subscribe((isConfirmed) => {
+      if (isConfirmed) {
+        this.isBusy = true;
+        this.fdService.deleteFixedDeposit(fdModel.id).subscribe(() => {
+          this.isBusy = false;
+        });
+      }
+    });
+  }
 
+  private showFDFormModal(fdId?: string) {
+    this.mdDialog.open(CreateFixedDepositModalComponent, {
+      data: { fdId },
+      panelClass: 'modal-sm'
+    }).afterClosed();
   }
 }
 
